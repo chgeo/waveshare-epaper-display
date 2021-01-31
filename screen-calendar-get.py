a7fe5ed550908f2acdb5634ff8e4ff9099ed36ad
Christian Georgi <chgeo@gmx.net>
2021-01-31 17:02:10 +0100

    Fix for recurring events

--- screen-calendar-get.py
+++ screen-calendar-get.py
@@ -1,4 +1,4 @@
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from time import time
 from tzlocal import get_localzone
 import locale
@@ -10,6 +10,7 @@ import caldav
 import vobject
 
 locale.setlocale(locale.LC_TIME, os.getenv('LC_TIME',''))
+localzone = get_localzone()
 
 # normalize dates, otherwise we can't compare them
 def byStartDate(event):
@@ -28,8 +29,10 @@ if user == '' or passwd == '' :
     sys.exit(1)
 
 template = 'screen-output-weather.svg'
-resultCal = None
 stale = True
+calDavServer = 'https://caldav.icloud.com/'
+weeksToSearch = 6
+resultCal = None
 
 if (os.path.isfile(os.getcwd() + '/calendar.cal')):
     print('Found cached calendar response')
@@ -38,27 +41,32 @@ if (os.path.isfile(os.getcwd() + '/calendar.cal')):
     stale=time() - os.path.getmtime(os.getcwd() + '/calendar.cal') > (1*60*60)
 
 if stale:
-    print('Calender data is stale, calling the Calendar API')
-    client = caldav.DAVClient('https://caldav.icloud.com/', username=user, password=passwd)
+    print('Calender data is stale, calling', calDavServer)
+    client = caldav.DAVClient(calDavServer, username=user, password=passwd)
     principal = client.principal()
+    start = datetime.utcnow()
     resultCal = vobject.iCalendar()
     for calendar in principal.calendars():
         events = calendar.date_search(
-            start=datetime.utcnow(), #datetime(2015, 1, 1)
-            end=datetime(2024, 1, 1), expand=False)
-        for event in events:
-            vevent = event.vobject_instance.vevent
+            start=start,
+            end=(start + timedelta(weeks=weeksToSearch)),
+            expand=True)  # expands recurring events to their _next_ occurrence
+        for comp in events:
+            vevent = comp.vobject_instance.vevent
+            # Expanded recurring events contain 2 DTSTAMP lines, which is illegal,
+            # leading to errors on .serialize(). Fix is to remove one such line (or all?)
+            vevent.remove(vevent.dtstamp)
             resultCal.add(vevent)
         with open('calendar.cal', 'w') as cal:
-            print(resultCal.serialize(), file=cal)
+            cal.write(resultCal.serialize())
 
 if not resultCal:
     print('No upcoming events found.')
 
 events = []
-for event in resultCal.components():
-    if (event.name == 'VEVENT'):
-        events.append(event)
+for comp in resultCal.components():
+    if (comp.name == 'VEVENT'):
+        events.append(comp)
 
 events.sort(key=byStartDate)
 
@@ -74,12 +82,12 @@ while (i < 3): # we have 3 slots in the UI
             day = start.strftime('%a %-d.%-m.')
             if (abs(start - end).days > 1): # show end date for multi-day events
                 day = day + end.strftime(' – %a %-d.%-m.')
-        else: # event within a day
-            start = start.astimezone(get_localzone())
+        else: # event with time, i.e. within a day
+            start = start.astimezone(localzone)
             day = start.strftime('%a %-d.%-m. %H:%M')
-        print(day, desc, start)
-    output = output.replace('CAL_'+str(i),day)
-    output = output.replace('CAL_DESC_'+str(i),desc)
+        print(day, desc)
+    output = output.replace('CAL_'+str(i), day)
+    output = output.replace('CAL_DESC_'+str(i), desc)
     i = i + 1
 
 codecs.open('screen-output-weather.svg', 'w', encoding='utf-8').write(output)
