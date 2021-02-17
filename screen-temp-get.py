bfd4351f92bf8a3a8d617bea7f01e09d9c51f629
Christian Georgi <chgeo@gmx.net>
2021-02-17 13:05:41 +0100

    Fix humidity data access

--- screen-temp-get.py
+++ screen-temp-get.py
@@ -13,13 +13,15 @@ addr = (
     else
     'CB8A04B0-864F-443D-85DE-D5158E9F9DC4'
 )
+data_uuid = '0000{0:x}-0000-1000-8000-00805f9b34fb'.format(0xfff2)
+sensor_filepath = os.getcwd() + '/sensor.ini'
 
 def float_value(nums):
     # check if temp is negative
     num = (nums[1]<<8)|nums[0]
     if nums[1] == 0xff:
         num = -( (num ^ 0xffff ) + 1)
-    return float(num)
+    return float(num) / 100
 
 temp = 0
 humidity = 0
@@ -28,22 +30,21 @@ async def fetch_data(addr: str):
     async with BleakClient(addr) as client:
         global temp, humidity
         await client.is_connected()
-        temp = await client.read_gatt_char('0000fff2-0000-1000-8000-00805f9b34fb')
-        temp = int(float_value(temp[0:2]) / 100)
-        humidity = await client.read_gatt_char('0000fff5-0000-1000-8000-00805f9b34fb')
-        humidity = int(float_value(humidity[0:2]) / 10)
-        with open('sensor.ini', 'w') as configfile:
+        data = await client.read_gatt_char(data_uuid)
+        temp = round(float_value(data[0:2]))
+        humidity = round(float_value(data[2:4]))
+        with open(sensor_filepath, 'w') as configfile:
             config = configparser.ConfigParser()
             config['Sensor'] = { 'temperature': temp, 'humidity' : humidity }
             config.write(configfile)
 
 stale = True
 
-if (os.path.isfile(os.getcwd() + '/sensor.ini')):
+if (os.path.isfile(sensor_filepath)):
     print('Found cached sensor data')
-    stale=time() - os.path.getmtime(os.getcwd() + '/sensor.ini') > (15*60)
+    stale=time() - os.path.getmtime(sensor_filepath) > (15*60)
     config = configparser.ConfigParser()
-    config.read('sensor.ini')
+    config.read(sensor_filepath)
     temp = config['Sensor']['temperature']
     humidity = config['Sensor']['humidity']
 
@@ -53,10 +54,11 @@ if stale:
         loop.run_until_complete(fetch_data(addr))
     except:
         print('Failed to get new sensor data, will use older data')
-        config = configparser.ConfigParser()
-        config.read('sensor.ini')
-        temp = config['Sensor']['temperature']
-        humidity = config['Sensor']['humidity']
+        if (os.path.isfile(sensor_filepath)):
+            config = configparser.ConfigParser()
+            config.read(sensor_filepath)
+            temp = config['Sensor']['temperature']
+            humidity = config['Sensor']['humidity']
 
 print('{0} °'.format(temp))
 print('Humidity: {0} %'.format(humidity))
